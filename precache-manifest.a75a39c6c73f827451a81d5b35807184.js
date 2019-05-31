0c476b5e3efa2684009a9a09e27f1a7816a9abfd
MarieThielens <thielens.marie@gmail.com>
2019-05-31 10:50:30 +0200

    Updates

---
+++ precache-manifest.a75a39c6c73f827451a81d5b35807184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a32bbded135f267bd7510b426ce851b",
+    "url": "/react-Jeux-Oxo/index.html"
+  },
+  {
+    "revision": "ded73604a3329bcbcdcb",
+    "url": "/react-Jeux-Oxo/static/css/main.628544f5.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/react-Jeux-Oxo/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "ded73604a3329bcbcdcb",
+    "url": "/react-Jeux-Oxo/static/js/main.ecd18eb0.chunk.js"
+  },
+  {
+    "revision": "f7aa77a9d5e2a0887ed3",
+    "url": "/react-Jeux-Oxo/static/js/runtime~main.789a0464.js"
+  }
+]);
